df832792ec71da84471168ecaea40ae292e66e48
Leonardo Machado Leite <leomleite@hotmail.com>
2020-04-26 21:35:20 -0300

    First commit

---
+++ backend.js
@@ -0,0 +1,37 @@
+var express = require("express")
+
+function add(req, res) {
+    console.log('A:' + String(req.query.A))
+    console.log('B:' + String(req.query.B))
+    res.send(200).json({
+        ans: Number(req.A) + Number(req.B)
+    });
+}
+
+function sub(a, b) {
+    return Number(a) - Number(b);
+}
+
+function mul(a, b) {
+    return Number(a) * Number(b);
+}
+
+function div(a, b) {
+    return Number(a) / Number(b);
+}
+
+function main() {
+
+    var app = express();
+    const port = 8082;
+  
+    // Routes
+    app.get('/Add?:A&:B', add);
+    app.get('/Sub', sub);
+    app.get('/Mul', mul);
+    app.get('/Div', div);
+
+    app.listen(port, () => console.log('Server is listening on port: ' + port));
+  }
+  
+  main()
